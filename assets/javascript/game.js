4e50d9d366bca43b8ff90c68f302ce7d8590c142
GBAkins <gavinakins29@gmail.com>
2019-05-08 16:33:17 -0500

    structured HTML and started naming variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// Variables 
+var wordOptions = ["superman", "batman", "spiderman"];
+var word = "";
+var letters = [];
+var blanks = 0;
+var blanksAndGuessed = [];
+var wrongGuesses = [];
+var wins = 0;
+var losses = 0;
+var guessesLeft = 8;
+
+// Functions
